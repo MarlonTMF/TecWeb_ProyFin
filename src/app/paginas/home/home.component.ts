a56059d2a981fe96a8f93871b4af70bbb4d817c4
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2024-12-02 10:38:02 -0400

    Inicio Configurado

---
+++ src/app/paginas/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ProductModel } from '../../modelos/reloj.model'; // Modelo de reloj
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
