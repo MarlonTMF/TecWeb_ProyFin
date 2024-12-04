258b8cba1ac7441c9fa7680c104107cd7175d0de
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2024-12-04 14:45:46 -0400

    CRUD Cliente implementado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AdminRelojesComponent } from './paginas/admin-relojes/admin-relojes.component';
 import { HomeComponent } from './paginas/home/home.component';
+import { AdminClientesComponent } from './paginas/admin-clientes/admin-clientes.component';
 
 
 export const routes: Routes = [
     {path:'', component:HomeComponent},
-    {path:'admin-relojes', component:AdminRelojesComponent}
+    {path:'admin-relojes', component:AdminRelojesComponent},
+    {path:'admin-clientes', component:AdminClientesComponent}
 ];
