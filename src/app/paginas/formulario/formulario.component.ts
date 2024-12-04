b5bdf3b91a6c06fc0103dcbf0429dfd69519d310
Marlon Tomas Marzo Fernandez <marlonmarzo2@gmail.com>
2024-12-04 16:36:39 -0400

    ImplementadoFIN_2

---
+++ src/app/paginas/formulario/formulario.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './formulario.component.html',
+  styleUrl: './formulario.component.scss'
+})
+export class FormularioComponent {
+  // Datos del usuario
+  user = {
+    nombre: '',
+    email: '',
+    telefono: '',
+  };
+
+  // Variable para mostrar mensaje de éxito
+  showSuccessMessage = false;
+
+  // Método para manejar el envío del formulario
+  onSubmit() {
+    this.showSuccessMessage = true;
+    console.log('Usuario registrado:', this.user);
+  }
+
+  // Método para redirigir a la página principal
+  goToHome() {
+    // Aquí puedes redirigir usando un router (ejemplo con Router.navigate(['/home']))
+    console.log('Navegando a la página principal');
+  }
+}
